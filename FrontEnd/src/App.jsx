50ae89e6400658638a5df50d38e9339ff95a51f6
Taimoo-r <taimoorking23102004@gmail.com>
2024-10-26 19:59:27 +0500

    Update App.jsx\

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,15 @@
-import Home from "../src/pages/Home"
+import Home from "./pages/Home"
 import React from 'react'
 import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Login from "../src/pages/Login"
-import SignUp from "../src/pages/SignUp"
-import Profile from "../src/pages/Profile"
-import Dashboard from "../src/pages/Dashboard"
-import MessageScreen from "../src/pages/MessageScreen"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import Dashboard from "./pages/Dashboard"
+import MessageScreen from "./pages/MessageScreen"
 import JobsSection from "./pages/JobsSection"
 import CommunitiesSection from "./pages/CommunitiesSection"
-import Projects from "../src/pages/Projects"
-import UserProfile from '../src/pages/UserProfile'
+import Projects from "./pages/Projects"
+import UserProfile from './pages/UserProfile'
 import { UserProvider } from "./userContext.jsx"
 
 export default function App() {
