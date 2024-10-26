589e51729b6af769abbe7d7c72379a98659e92de
Taimoo-r <taimoorking23102004@gmail.com>
2024-10-26 20:51:07 +0500

    Create vite.config.js

---
+++ FrontEnd/vite.config.js
@@ -0,0 +1,33 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+  
+// })
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Only if you need to externalize it
+    },
+  },
+})
+
+
