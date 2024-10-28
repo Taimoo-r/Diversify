8913941502511ec712cac51f058efaefce22eef8
Taimoo-r <taimoorking23102004@gmail.com>
2024-10-28 10:33:55 +0500

    Update multer.middleware.js

--- BackEnd/src/middleware/multer.middleware.js
+++ BackEnd/src/middleware/multer.middleware.js
@@ -1,18 +1,9 @@
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
-
-// Define the directory path
-const uploadDir = 'uploads/resumes';
-
-// Check if directory exists, create if it doesn't
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir); // Set destination for uploaded files
+        cb(null, 'uploads/resumes'); // Destination for uploaded files
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
