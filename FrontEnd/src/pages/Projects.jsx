102832d51a62e70f2d510016dc6d75cb2356e53a
Taimoo-r <taimoorking23102004@gmail.com>
2024-11-05 22:22:22 +0500

    Update Projects.jsx

--- FrontEnd/src/pages/Projects.jsx
+++ FrontEnd/src/pages/Projects.jsx
@@ -5,7 +5,7 @@ export default function Component() {
     <div className="flex items-center justify-center min-h-screen bg-purple-300">
       <div className="bg-white bg-opacity-80 backdrop-blur-md rounded-lg shadow-lg p-8 text-center">
         <h1 className="text-4xl font-bold text-gray-800">
-          Best of Luck 🍀
+         Coming Soon 🌟 Stay Tunned.......
         </h1>
       </div>
     </div>
